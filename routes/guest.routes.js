a8a52850e97e175d40315da9d222ec2c47d99b19
Anna Kazulak <anna.kazulak@gmail.com>
2024-01-17 09:45:14 +0100

    - Fix issue with image dimensions not updating when image is updated - Ensure user permissions are checked before updating the guest - Refactor code for improved readability

--- routes/guest.routes.js
+++ routes/guest.routes.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const cloudinary = require("cloudinary").v2;
 const mongoose = require("mongoose");
-const { isAuthenticated } = require("../middleware/jwt.middleware");
-const { checkOwnership } = require("../middleware/authorization");
+const {
+  isAuthenticated
+} = require("../middleware/jwt.middleware");
+const {
+  checkOwnership
+} = require("../middleware/authorization");
 
 const Event = require("../models/Event.model");
 const Guest = require("../models/Guest.model");
@@ -22,8 +26,9 @@ router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
 
   // call the getImage method to get the dimensions of the image
   cloudinary.uploader.explicit(
-    imageUrl,
-    { type: "fetch" },
+    imageUrl, {
+      type: "fetch"
+    },
     function (error, result) {
       if (error) {
         console.log(error);
@@ -40,8 +45,15 @@ router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
 });
 
 router.post("/guests", isAuthenticated, (req, res, next) => {
-  const { name, description, imageUrl, eventId, imageWidth, imageHeight } =
-    req.body;
+  const {
+    name,
+    description,
+    imageUrl,
+    eventId,
+    imageWidth,
+    imageHeight
+  } =
+  req.body;
   console.log(req.body);
 
   const userId = req.payload._id;
@@ -58,7 +70,9 @@ router.post("/guests", isAuthenticated, (req, res, next) => {
   Guest.create(newGuest)
     .then((newGuest) => {
       return Event.findByIdAndUpdate(eventId, {
-        $push: { guests: newGuest._id },
+        $push: {
+          guests: newGuest._id
+        },
       });
     })
     .then((response) => res.json(response))
@@ -73,7 +87,9 @@ router.post("/guests", isAuthenticated, (req, res, next) => {
 
 // GET /api/guests  Retrieves all of the guests
 router.get("/guests", isAuthenticated, checkOwnership, (req, res, next) => {
-  Guest.find({ creator: req.payload._id })
+  Guest.find({
+      creator: req.payload._id
+    })
     .then((guests) => res.json(guests))
     .catch((err) => {
       console.error("Error getting list of guests...", err);
@@ -86,21 +102,29 @@ router.get("/guests", isAuthenticated, checkOwnership, (req, res, next) => {
 
 //  GET /guests/:guestId  Retrieve details of a single guest by their ID.
 router.get("/guests/:guestId", isAuthenticated, (req, res, next) => {
-  const { guestId } = req.params;
+  const {
+    guestId
+  } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(guestId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
+    res.status(400).json({
+      message: "Specified id is not valid"
+    });
     return;
   }
 
   Guest.findById(guestId)
     .then((guest) => {
       if (!guest) {
-        res.status(404).json({ message: "Guest not found" });
+        res.status(404).json({
+          message: "Guest not found"
+        });
       } else if (guest.creator.toString() !== req.payload._id) {
         res
           .status(403)
-          .json({ message: "You are not authorized to view this guest" });
+          .json({
+            message: "You are not authorized to view this guest"
+          });
       } else {
         res.json(guest);
       }
@@ -114,59 +138,94 @@ router.get("/guests/:guestId", isAuthenticated, (req, res, next) => {
     });
 });
 
-router.put("/guests/:guestId", isAuthenticated, (req, res, next) => {
-  const { guestId } = req.params;
-  const { name, description, imageUrl } = req.body;
+router.put("/guests/:guestId", isAuthenticated, async (req, res, next) => {
+  try {
+    const {
+      guestId
+    } = req.params;
+    const {
+      name,
+      description,
+      imageUrl
+    } = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(guestId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
-
-  // Check if the user has permission to update the guest
-  Guest.findById(guestId)
-    .then((guest) => {
-      if (!guest) {
-        res.status(404).json({ message: "Guest not found" });
-      } else if (guest.creator.toString() !== req.payload._id) {
-        res
-          .status(403)
-          .json({ message: "You are not authorized to update this guest" });
-      } else {
-        const newDetails = { name, description, imageUrl };
-
-        Guest.findByIdAndUpdate(guestId, newDetails, { new: true })
-          .then((updatedGuest) => {
-            if (!updatedGuest) {
-              res.status(404).json({ message: "Guest not found" });
-            } else {
-              res.json(updatedGuest);
-            }
-          })
-          .catch((err) => {
-            console.error("Error updating guest...", err);
-            res.status(500).json({
-              message: "Error updating guest",
-              error: err,
-            });
-          });
-      }
-    })
-    .catch((err) => {
-      console.error("Error getting guest details...", err);
-      res.status(500).json({
-        message: "Error getting guest details",
-        error: err,
+    if (!mongoose.Types.ObjectId.isValid(guestId)) {
+      return res.status(400).json({
+        message: "Specified id is not valid"
       });
+    }
+
+    // Check if the user has permission to update the guest
+    const guest = await Guest.findById(guestId);
+
+    if (!guest) {
+      return res.status(404).json({
+        message: "Guest not found"
+      });
+    } else if (guest.creator.toString() !== req.payload._id) {
+      return res
+        .status(403)
+        .json({
+          message: "You are not authorized to update this guest"
+        });
+    }
+
+    const newDetails = {
+      name,
+      description,
+      imageUrl
+    };
+
+    // Update the guest with the new details
+    const updatedGuest = await Guest.findByIdAndUpdate(guestId, newDetails, {
+      new: true,
     });
+
+    // Get the dimensions of the updated image
+    const result = await cloudinary.uploader.explicit(
+      updatedGuest.imageUrl, {
+        type: "fetch"
+      },
+      function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          return result;
+        }
+      }
+    );
+
+    // Update the document in the database with the new dimensions
+    await Guest.findByIdAndUpdate(
+      guestId, {
+        imageWidth: result.width,
+        imageHeight: result.height
+      }, {
+        new: true
+      }
+    );
+
+    res.json(updatedGuest);
+  } catch (err) {
+    console.error("Error updating guest...", err);
+    res.status(500).json({
+      message: "Error updating guest",
+      error: err,
+    });
+  }
 });
+
 
 // DELETE /guests/:guestId Delete a guest by their ID.
 router.delete("/guests/:guestId", isAuthenticated, (req, res, next) => {
-  const { guestId } = req.params;
+  const {
+    guestId
+  } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(guestId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
+    res.status(400).json({
+      message: "Specified id is not valid"
+    });
     return;
   }
 
@@ -174,18 +233,26 @@ router.delete("/guests/:guestId", isAuthenticated, (req, res, next) => {
   Guest.findById(guestId)
     .then((guest) => {
       if (!guest) {
-        res.status(404).json({ message: "Guest not found" });
+        res.status(404).json({
+          message: "Guest not found"
+        });
       } else if (guest.creator.toString() !== req.payload._id) {
         res
           .status(403)
-          .json({ message: "You are not authorized to delete this guest" });
+          .json({
+            message: "You are not authorized to delete this guest"
+          });
       } else {
         Guest.findByIdAndRemove(guestId)
           .then((deletedGuest) => {
             if (!deletedGuest) {
-              res.status(404).json({ message: "Guest not found" });
+              res.status(404).json({
+                message: "Guest not found"
+              });
             } else {
-              res.json({ message: "Guest deleted successfully" });
+              res.json({
+                message: "Guest deleted successfully"
+              });
             }
           })
           .catch((err) => {
