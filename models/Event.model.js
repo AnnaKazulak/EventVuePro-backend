778d04302a3667a9a996a1081713223433f913cf
Anna Kazulak <anna.kazulak@gmail.com>
2024-01-18 16:38:56 +0100

    add img galery to the event model

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,23 +1,35 @@
-const { Schema, model } = require("mongoose");
+const {
+  Schema,
+  model
+} = require("mongoose");
 
-const eventSchema = new Schema(
-  {
-    title: { type: String, required: [true, "Event title is required"] },
-    description: String,
-    date: Date,
-    time: String,
-    location: String,
-    imageUrl: { type: String, required: false },
-    guests: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Guest",
-      },
-    ],
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Event title is required"]
   },
-  {
-    timestamps: true,
-  }
-);
+  description: String,
+  date: Date,
+  time: String,
+  location: String,
+  imageUrl: {
+    type: String,
+    required: false
+  },
+  gallery: [{
+    galleryImageUrl: {
+      type: String,
+      required: false,
+    },
+    imageWidth: Number,
+    imageHeight: Number,
+  }, ],
+  guests: [{
+    type: Schema.Types.ObjectId,
+    ref: "Guest",
+  }, ],
+}, {
+  timestamps: true,
+});
 
 module.exports = model("event", eventSchema);
