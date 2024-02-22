d795dffe47c8fb038f49cfe0414a6f0ff988c047
Anna Kazulak <anna.kazulak@gmail.com>
2024-02-22 14:17:11 +0100

    add inveitedGuest to Email model, deleted comments

--- models/EmailInvitation.model.js
+++ models/EmailInvitation.model.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 
-// Define the schema for event invitations
 const eventInvitationSchema = new mongoose.Schema({
     eventId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Event'
-    }, // Reference to the event
-    recipientEmail: String, // Email of the recipient
+    }, 
+    recipientEmail: String, 
     rsvpResponse: String, // RSVP response from the recipient (e.g., 'attending', 'not_attending','pending')
     invitedGuests: [{ 
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Guest'
-    }] // Array of guest IDs who were invited to the event
+    }], 
+    invitedGuest: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Guest'
+    } 
 });
 
-// Create a Mongoose model based on the schema
+
 const EventInvitation = mongoose.model('EventInvitation', eventInvitationSchema);
 
 module.exports = EventInvitation;
